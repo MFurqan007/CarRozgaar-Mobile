e5c4bc5c8b4220e3f5f583c6e7b203c3a01d6c49
MFurqan007 <mfurqannasir24@gmail.com>
2024-03-28 23:23:42 +0500

    upload video 6

--- src/app/(portal)/Home/UploadVid/page.js
+++ src/app/(portal)/Home/UploadVid/page.js
@@ -1,17 +1,17 @@
 "use client"
 import React from 'react'
-import { useRouter} from 'next/navigation';
+// import { useRouter} from 'next/navigation';
 import Upload from '@/components/Upload';
   
 
 export default function page() {
-    const router = useRouter();
+    // const router = useRouter();
     
   return (
     <div className='flex flex-col gap-4'>
         <button 
             className='btn'
-            onClick={() => router.push('./')}
+            // onClick={() => router.push('./')}
         >Back</button>
         <Upload/>
     </div>
